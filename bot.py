639a545f135a3af096cb5b962301a9b9fdf33b5e
Stian Kodehode <stiant.kodehode@gmail.com>
2022-12-26 02:33:30 +0100

    added rare reply to person

--- bot.py
+++ bot.py
@@ -3,7 +3,10 @@ from dotenv import load_dotenv
 from discord.ext import commands
 import discord
 import time
+import random
+
 from src.get_joke import get_joke
+from src.munki_quote import munki_quote
 
 
 load_dotenv()
@@ -12,12 +15,29 @@ CHANNEL_ID = 1056406916424810496
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
+#
+# EVENTS
+#
+
 @bot.event
 async def on_ready():
     print("PagMan is ready!")
     channel = bot.get_channel(CHANNEL_ID)
     await channel.send("<:PagMan:1056702924253839421> is here BAYBEEE!")
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.author.name == "SmarDon":
+        chance = random.random()
+        if chance < 0.05:
+            await message.channel.send(munki_quote())
+
+#
+# COMMANDS
+#     
+    
 @bot.command()
 async def hello(ctx):
     await ctx.send(f"Hello {ctx.author.name}")
@@ -32,8 +52,6 @@ async def joke(ctx):
         time.sleep(2)
         await ctx.send(joke["delivery"])
         
-        
-    return
 
 
 
---
+++ src/munki_quote.py
@@ -0,0 +1,6 @@
+import random
+
+def munki_quote():
+    munkiquotes = ["Tjasid", "e d lov?", "e d lov å si?", "mystiske bugs", "jyslig forbanna", "jyslig arg", "fytt i micca?"]
+    random_quote = random.choice(munkiquotes)
+    return random_quote
